13bea38123593dd37356601fe3d3ce75e5458f37
Myo Win <60438355+Dev-MyoWin@users.noreply.github.com>
2021-01-17 16:54:48 +0630

    Update CommentController.php

--- app/Http/Controllers/CommentController.php
+++ app/Http/Controllers/CommentController.php
@@ -17,9 +17,12 @@ class CommentController extends Controller
         if($validator->fails ()) {
             return back()-> withErrors($validator);
             }
-$comment = new Comment;
-$comment->content = request()->content; $comment->article_id = request()->article_id;$comment->user_id = auth()->user()->id; $comment->save();
-return back(); }
+            $comment = new Comment;
+            $comment->content = request()->content; 
+            $comment->article_id = request()->article_id;
+            $comment->user_id = auth()->user()->id; 
+            $comment->save();
+            return back(); }
 
 public function delete($id) {
     $comment = Comment::find($id);
